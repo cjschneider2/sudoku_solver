74792a41f56f4e2dc9c016d78e85c2c11d626e48
cjschneider2 <cjschneider2@gmail.com>
2015-05-07 14:31:47 +0200

    (feat) added a new test with a solved solution, adding to Board::is_valid_solution()

--- src/board.rs
+++ src/board.rs
@@ -3,6 +3,49 @@ pub struct Board {
 }
 
 impl Board {
+    /// Checks to see if the board is a valid sudoku solution.
+    /// Three "rules" need to be checked:
+    ///     column unique: Each column has to have the #'s 1..9 w/o repeats
+    ///     row unique: Each row has to have the #'s 1..9 w/o repeats
+    ///     zone unique: Each 3x3 area needs to have the #'s 1..9 w/o repeats
+    pub fn is_valid_solution(&self) -> bool {
+        // Check columns
+        for col in 0..9 {
+            let check = Board::check_column(&self, col);
+            match check {
+                None => (),
+                Some(remaining) => {
+                    println!("The following numbers are remaining: {:?}",
+                             remaining)
+                }
+            }
+        }
+        // Check rows
+        // Check zones
+        true
+    }
+
+    fn check_column(&self, col_num: usize) -> Option<Vec<usize>> {
+        Some(vec!{1,2,3})
+    }
+
+    fn check_row(&self, row_num: usize) -> Option<Vec<usize>> {
+        None
+    }
+
+    /// The zones are numbered 0..8 in this order:
+    ///     +-+-+-+
+    ///     |0|1|2|
+    ///     +-+-+-+
+    ///     |3|4|5|
+    ///     +-+-+-+
+    ///     |6|7|8|
+    ///     +-+-+-+
+    fn check_zone(&self, zone_num: usize) -> Option<Vec<usize>> {
+        None
+    }
+
+    /// Displays the board in a wonderfully retro ASCII style.
     pub fn display(&self) {
         println!("+---+---+---+");
         for i in 0..3 {
@@ -69,4 +112,8 @@ pub fn new_empty( ) -> Board {
         [ 0,0,0, 0,0,0, 0,0,0],
         [ 0,0,0, 0,0,0, 0,0,0],
         [ 0,0,0, 0,0,0, 0,0,0]] }
+}
+
+pub fn new_with_entries( input_entries: [[usize; 9]; 9] ) -> Board {
+    Board { entries: input_entries }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,32 @@ fn main() {
 #[cfg(test)]
 mod test {
     #[test]
-    fn print_empty_board() {
-        let a_board = board::new_empty();
+    fn display_empty_board() {
+        let a_board = super::board::new_empty();
         a_board.display();
+    }
+    #[test]
+    fn empty_not_valid_solution() {
+        let empty_board = super::board::new_empty();
+        let value = empty_board.is_valid_solution();
+        assert_eq!(value, false);
+    }
+    #[test]
+    fn test_a_valid_solution() {
+        let a_board = super::board::new_with_entries(
+            [[ 4,5,3, 9,2,7, 1,8,6],
+             [ 6,8,1, 4,3,5, 7,9,2],
+             [ 7,9,2, 6,8,1, 3,4,5],
+
+             [ 5,1,4, 7,9,3, 6,2,8],
+             [ 3,2,6, 5,1,8, 4,7,9],
+             [ 8,7,9, 2,6,4, 5,1,3],
+
+             [ 9,3,3, 8,4,6, 2,5,1],
+             [ 1,4,8, 3,5,2, 9,6,7],
+             [ 2,6,5, 1,7,9, 8,3,4]]);
+        let value = a_board.is_valid_solution();
+        assert_eq!(value, true);
     }
 }
 
