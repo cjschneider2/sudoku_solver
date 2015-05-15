b637d0bc19e8b3702c5995cfafc21c30d59e206b
cjschneider2 <cjschneider2@gmail.com>
2015-05-15 12:01:24 +0200

    rewrote the solution code to be simpler

--- src/solver.rs
+++ src/solver.rs
@@ -8,46 +8,10 @@ use board::Board;
 #[derive(Clone, Debug)]
 pub struct SolState {
     pub board: Board,
-    pub possi: Vec<usize>,       // the Vector of possible valid entries
     pub entry: (usize, usize),    // the entry position that was changed
 }
 
-impl SolState {
-    pub fn next_state(&self) -> Option<SolState> {
-        let mut ret_state = self.clone();
-        // Find the next empty X and Y coordinates
-        let (mut e_x, mut e_y, mut empty) = (0,0,false);
-        match ret_state.board.next_empty() {
-            Some((x,y)) => { e_x = x; e_y = y; },
-            None => {empty = true;},
-        };
-        println!("empty at {},{}", e_x, e_y);
-        // more possibilities
-        if ret_state.possi.len() > 0 {
-            if !empty {
-                println!("vectors, has space");
-                let (x, y) = ret_state.entry;
-                ret_state.board.insert(ret_state.possi.pop().unwrap(), x, y);
-                return Some(ret_state);
-            } else {
-                println!("vectors, no space");
-                return None;
-            }
-        } else {
-            if !empty {
-                println!("no vectors, space");
-                ret_state.possi = ret_state.board.get_valid_pos(e_x,e_y);
-                ret_state.entry = (e_x,e_y);
-                return Some(ret_state);
-            } else {
-                println!("no vectors, no space");
-                return None;
-            }
-        }
-    }
-}
-
-/// The back-tracing algorithm functions as follows:
+/// The backtracking algorithm functions as follows:
 /// Empty spaces will generate a vector of possible entries for
 /// that position. The back-tracing algorithm then generates a new
 /// solution state, popping the current value off of the list of possible
@@ -63,21 +27,26 @@ impl SolState {
 ///    possibilities, and yet still more spaces then the function will return a
 ///    None value and the process will continue again until there are no other
 ///    possibilities left.
-pub fn solve_with_backtracing(in_state: Box<SolState>) -> Option<Box<SolState>> {
-    // Create/initialize a new solution state
-    let new_state = Box::new(in_state.next_state());
-    println!("{:?}",new_state);
-    match *new_state {
-        Some(state) => {
-            let (_, solved) = state.board.is_valid_solution();
-            if solved {
-                return Some(Box::new(state));
-            } else {
-                return solve_with_backtracing(Box::new(state));
-            }
-        },
-        None => { return None; },
+pub fn solve_with_backtracing(state: Box<Board>) -> bool {
+    let mut row = 0;
+    let mut col = 0;
+
+    match state.next_empty() {
+        Some((x,y)) => { row = x; col = y; }
+        None => { return true; } // the board is full
     }
+
+    for num in 1..10 {
+        // check for conflicts and insert a new number if okay
+        if {
+            state.insert(num, row, col);
+            // if solved then return true
+            if solve_with_backtracing(state) { return true; }
+            // else we'll undo our changes
+            state.insert(0, row, col);
+        }
+    }
+    return false; // initate backtracking
 }
 
 #[cfg(test)]
@@ -97,14 +66,9 @@ mod tests {
              [ 9,3,7, 8,4,6, 2,5,1],
              [ 1,4,8, 3,5,2, 9,6,7],
              [ 2,6,5, 1,7,9, 8,3,4]]);
-        let init_state = Box::new(super::SolState {
-            board: a_board,
-            possi: vec!(),
-            entry: (1,1),
-        });
-        let out_state = super::solve_with_backtracing(init_state);
+        let out_state = super::solve_with_backtracing(a_board);
         println!("{:?}", out_state);
-        let (valid, solved) = out_state.unwrap().board.is_valid_solution();
+        let (valid, solved) = out_state.is_valid_solution();
         assert!(valid && solved);
     }
 }
