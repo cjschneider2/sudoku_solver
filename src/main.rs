2952516b8b35dd9fd18f0752e3b5520dafb38a9a
cjschneider2 <cjschneider2@gmail.com>
2015-05-20 22:28:33 +0200

    added some information about test cases for some sudoku puzzles to solve.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
-#![allow(dead_code)]
+#![macro_use]
+extern crate glium;
+extern crate glutin;
+
+use glium::DisplayBuild;
+use glium::Surface;
+use glium::Program;
 
 mod board;
 mod solver;
 
 fn main() {
-    /*
-    let a_board = board::new_with_entries(
-        [[ 0,5,3, 9,2,7, 1,8,6],
-         [ 6,0,1, 4,3,5, 7,9,2],
-         [ 7,9,2, 6,8,1, 3,4,5],
+    let width:u32 = 1024;
+    let height:u32 = 768;
+    let display = glutin::WindowBuilder::new()
+        .with_dimensions(width, height)
+        .with_title(format!("Sudoku"))
+        .build_glium()
+        .unwrap();
 
-         [ 5,1,4, 7,9,3, 6,2,8],
-         [ 3,2,6, 5,1,8, 4,7,9],
-         [ 8,7,9, 2,6,4, 5,1,3],
-
-         [ 9,3,7, 8,4,6, 2,5,1],
-         [ 1,4,8, 3,5,2, 9,6,7],
-         [ 2,6,5, 1,7,9, 8,3,4]]);
-    */
-    let mut a_board = Box::new(board::new_empty());
-    let result = solver::solve_with_backtracing(&mut a_board);
-    println!("Solved? {}\n {:?}", result, a_board);
+    let (verts, idx) = square::geometry(&display);
 }
