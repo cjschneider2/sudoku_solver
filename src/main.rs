a609501e6ff1c75e41fef1bdfc4836e98f70f296
cjschneider2 <cjschneider2@gmail.com>
2015-05-22 15:57:36 +0200

    working on the GUI using glium

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+#![allow(dead_code, unused_imports)]
 #![macro_use]
 extern crate glium;
 extern crate glutin;
 
-use glium::DisplayBuild;
-use glium::Surface;
+use glium::{Surface, DisplayBuild};
 use glium::Program;
 
 mod board;
@@ -23,28 +23,47 @@ use solver::solve_with_backtracing;
 
 
 fn main() {
-    /*
-    let width:u32 = 1024;
-    let height:u32 = 768;
+    let width:u32 = 512;
+    let height:u32 = 512;
+
     let display = glutin::WindowBuilder::new()
         .with_dimensions(width, height)
         .with_title(format!("Sudoku"))
         .build_glium()
         .unwrap();
-    let (verts, idx) = square::geometry(&display);
-    */
 
+    loop {
+
+        // Produces a new 'Frame'
+        let mut target = display.draw();
+
+        // set the background to blue
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+        // We've finished drawing, this will distroy the frame
+        // object and copies it into the window.
+        target.finish();
+
+        //shutdown code
+        if display.is_closed() {
+            println!("Display Closed");
+            break;
+        }
+    }
+}
+
+fn solve_all() {
     let mut all_solved = false;
     // we'll enumerate our sample sudoku tests from the 'tests.txt' file,
     // one sudoku puzzle per line.
     let path = Path::new("./tests/tests.txt"); // starts in the main dir
     let mut file = match File::open(&path) {
         Ok(file) => file,
-        Err(E) => panic!("couldn't open file {}: {}", path.display(),
-                        Error::description(&E)),
+        Err(error) => panic!("couldn't open file {}: {}", path.display(),
+                        Error::description(&error)),
     };
     let mut string = String::new();
-    file.read_to_string(&mut string);
+    let _ = file.read_to_string(&mut string);
     let entries: Vec<&str> = string.split_terminator("\r\n").collect();
 
     let mut solved_cnt = 0;
@@ -59,5 +78,4 @@ fn main() {
     }
     if cnt == solved_cnt { all_solved = true; }
     assert!(all_solved);
-
 }
